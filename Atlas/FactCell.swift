d4d10d1cd2669213c3eadea90a21dbfcb3dcddc7
Mathew Spolin <matt@automatt.com>
2015-04-14 19:28:54 -0700

    Try to fix label constraint issue

--- Atlas/FactCell.swift
+++ Atlas/FactCell.swift
@@ -12,7 +12,7 @@ class FactCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
-      
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
